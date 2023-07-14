175dd3618411dc2a3155d7030909a4f127a25206
HACO <jasonytonlinecomeandsee@gmail.com>
2023-07-14 23:17:57 +0800

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,37 +3,6 @@ import styles from "@/pages/index.module.css";
 
 export default function Home() {
     return (
-        // <div className="h-[100svh] bg-[url('/images/home-bg.jpg')] bg-cover">
-        //     <div className="h-full brightness-75">
-        //         <p className="opacity-100">
-        //             TEST
-        //         </p>
-        //     </div>
-        // </div>
-        // <div class="relative h-[100svh] max-h-[100svh]">
-        //     <div class="absolute inset-0 bg-black opacity-70"></div>
-        //     <img
-        //         class="w-full h-full object-cover"
-        //         src="/images/home-bg-small.jpg"
-        //         alt="Background image"
-        //     />
-        //     <div class="absolute">123</div>
-        // </div>
-
-        // <div class="relative h-[100svh]">
-        //     <img
-        //         class="w-full h-full object-cover"
-        //         src="/images/home-bg.jpg"
-        //         alt="Home BG"
-        //     />
-        //     <div class="absolute inset-0 backdrop-filter backdrop-brightness-[.40]">
-        //         <div className="pt-[170px] backdrop-brightness-95">
-        //             <h1 class="text-white font-major-mono text-[84px]">
-        //                 Dev-in.tw
-        //             </h1>
-        //         </div>
-        //     </div>
-        // </div>
         <div className={styles["background-image"] + " h-[100svh]"}>
             <div className={styles.content + " h-full"}>
                 <div className="h-[calc(100%-80px)] pt-[170px]">
