6e429f60d52445d4c57cd3dd35aff658db26f7bd
HACO <jasonytonlinecomeandsee@gmail.com>
2023-07-15 19:14:53 +0800

    Fixed DOM property

--- src/components/icons/ArrowDown.jsx
+++ src/components/icons/ArrowDown.jsx
@@ -33,9 +33,9 @@ export default function ArrowDown() {
                     width="85"
                     height="87.9166"
                     filterUnits="userSpaceOnUse"
-                    color-interpolation-filters="sRGB"
+                    colorInterpolationFilters="sRGB"
                 >
-                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
                     <feColorMatrix
                         in="SourceAlpha"
                         type="matrix"
